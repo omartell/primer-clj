477757c837f56ea8f6489cba721b738f5d27b409
Oliver Martell <oliver.martell@gmail.com>
2015-08-23 12:38:11 +0100

    Set up project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject primer "0.1.0-SNAPSHOT"
+  :description "Prime numbers generator"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main primer.core
+  :dependencies [[org.clojure/clojure "1.6.0"]])
